ed57949811762596e3b17a35e729ca04463ee18a
Michael M Slusarz <slusarz@curecanti.org>
2011-02-10 16:56:13 -0700

    Step 1 of Horde_Block refactoring: H4-like class loading/naming

--- lib/Block/Page.php
+++ lib/Block/Page.php
@@ -1,39 +1,50 @@
 <?php
-
-$block_name = _("Wiki page");
-
 /**
- * This class extends Horde_Block:: to display a Wiki page.
+ * A block to display a Wiki page.
  *
  * Copyright 2004-2011 The Horde Project (http://www.horde.org/)
  *
  * See the enclosed file COPYING for license information (GPL). If you
  * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
  *
- * @author  Jason Felice <jason.m.felice@gmail.com>
- * @package Horde_Block
+ * @author Jason Felice <jason.m.felice@gmail.com>
  */
-class Horde_Block_Wicked_page extends Horde_Block
+class Wicked_Block_Page extends Horde_Block
 {
-    protected $_app = 'wicked';
+    /**
+     */
+    public function getName()
+    {
+        return _("Wiki page");
+    }
 
+    /**
+     */
     protected function _title()
     {
         $page = Wicked_Page::getPage($this->_params['page']);
         return htmlspecialchars($page->pageName());
     }
 
+    /**
+     */
     protected function _content()
     {
         $page = Wicked_Page::getPage($this->_params['page']);
         return $page->render(Wicked::MODE_BLOCK);
     }
 
+    /**
+     */
     protected function _params()
     {
-        return array('page' => array('type' => 'text',
-                                     'name' => _("Name of wiki page to display"),
-                                     'default' => 'WikiHome'));
+        return array(
+            'page' => array(
+                'type' => 'text',
+                'name' => _("Name of wiki page to display"),
+                'default' => 'WikiHome'
+            )
+        );
     }
 
 }
