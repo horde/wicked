e75c171382587b251b7afa0485b1693060f34351
Torben Dannhauer <torben@dannhauer.de>
2023-04-10 22:07:53 +0200

    Update NewPage.php

--- lib/Page/NewPage.php
+++ lib/Page/NewPage.php
@@ -65,8 +65,11 @@ class Wicked_Page_NewPage extends Wicked_Page
     /**
      * Send them back whence they came if they aren't allowed to edit
      * this page.
+     *
+     * $param integer $mode    The page render mode.
+     * $param array   $params  Any page parameters.
      */
-    public function preDisplay()
+    public function preDisplay($mode, $params)
     {
         if (!strlen($this->referrer())) {
             $GLOBALS['notification']->push(_("Page name must not be empty"));
