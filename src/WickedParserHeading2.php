4157f9cf5e718cafa05e8cb6c0f7a6300e7afbd5
Ralf Lang <ralf.lang@ralf-lang.de>
2025-05-25 23:37:20 +0200

    Add horde/wicked specific rules to WickedEngine

---
+++ src/WickedParserHeading2.php
@@ -0,0 +1,44 @@
+<?php
+namespace Horde\Wicked;
+use Horde\Text\Wiki\WikiParserBase;
+use Horde\Text\Wiki\DefaultParserHeading;
+use Wicked;
+
+
+/**
+ * Parsers class as a complement to the Header2 renderer.
+ *
+ * Works around broken parser, adding additional spaces inside headers.
+ *
+ * @package Wicked
+ */
+class WickedParserHeading2 extends WikiParserBase
+{
+    protected $wrappedParserClass;
+    protected $wrappedParser;
+    /**
+     * TODO: This is a lazy implementation. We should rather make this a proxy
+     * - Leaving the input untouched and forwarding it to the proper backend parser
+     * - Only if the backend parser needs amending like "default" does, we should first manipulate the input.
+     */
+    public function __construct(WickedEngine $obj)
+    {
+        parent::__construct($obj);
+        $this->wrappedParserClass = $obj->parserPrefix . 'ParserHeading';
+        $this->wrappedParser = new $this->wrappedParserClass($obj);
+        if ($this->wrappedParser instanceof DefaultParserHeading) {
+//            $this->wrappedParser->regex = '/^(\++ *(.*)/mu';
+        }
+    }
+    public function process($matches)
+    {
+        return $this->wrappedParser->process($matches);
+    }
+
+    public function parse()
+    {
+        // We need to parse the text with the wrapped parser.
+        // This is a workaround for the broken parser.
+        return $this->wrappedParser->parse();
+    }
+}
