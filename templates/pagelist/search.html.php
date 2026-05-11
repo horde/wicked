feb0acee32b0c25646fb013feb981fc271bbe696
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-11 20:03:24 +0200

    feat: Add search page to sidebar
    Partially adresses horde/wicked#20

--- templates/pagelist/search.html.php
+++ templates/pagelist/search.html.php
@@ -1,1 +1,10 @@
-<h1 class="header"><?php echo _("Search Results") ?></h1>
+<h1 class="header"><?php echo _("Search") ?></h1>
+
+<form method="get" action="<?php echo Wicked::url('Search') ?>">
+  <input type="hidden" name="page" value="Search" />
+  <div class="horde-content">
+    <label for="wicked-search-input"><?php echo _("Search pages for") ?>:</label>
+    <input type="text" id="wicked-search-input" name="searchfield" value="<?php echo htmlspecialchars($this->searchtext ?? '') ?>" size="40" />
+    <input type="submit" class="horde-default" value="<?php echo _("Search") ?>" />
+  </div>
+</form>
